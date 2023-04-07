be24e5e9404f383bd3592a22f184b0c7de3d92ad
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-04-07 07:38:18 -0300

    Post "/sign-up"

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from "express";
 import cors from "cors";
 
-const 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+const usuarios = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    usuarios.push(req.body);
+    res.send("OK");
+});
 
 const PORT = 5000;
-.listen(PORT, ()=>console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, ()=>console.log(`Servidor rodando na porta ${PORT}`));
